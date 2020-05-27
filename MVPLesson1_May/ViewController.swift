8bfdf8cddb904c8891c2cb74731c85c7c0f4d12b
dur5t <mukasor@gmail.com>
2020-05-27 10:44:48 +0300

    Initial Commit

---
+++ MVPLesson1_May/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVPLesson1_May
+//
+//  Created by Алексей Чигарских on 27.05.2020.
+//  Copyright © 2020 Алексей Чигарских. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
